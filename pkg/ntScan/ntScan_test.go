827c6dc455fdfa694d0469bfb34de8629dc8ab5c
djian01 <dennis.jian01@gmail.com>
2024-09-20 08:26:39 +0000

    20240920_06

--- pkg/ntScan/ntScan_test.go
+++ pkg/ntScan/ntScan_test.go
@@ -25,28 +25,3 @@ func Test_ScanMTURun(t *testing.T) {
 		fmt.Println(err)
 	}
 }
-
-// func Test_ScanTcpRun(t *testing.T) {
-
-// 	recording := true
-// 	destHost := "google.com"
-// 	PortsArgs := []string{"22", "80", "443", "100-120", "1500"}
-// 	timeout := 4
-
-// 	// turn PortsArgs into []int{}
-// 	Ports := []int{}
-
-// 	for _, PortString := range PortsArgs {
-// 		Valid, Port := tcpscan.IsValidInput(PortString)
-// 		if !Valid {
-// 			fmt.Println("The input Port is not a valid port")
-// 		} else {
-// 			Ports = append(Ports, Port...)
-// 		}
-// 	}
-
-// 	err := ntScan.ScanTcpRun(recording, destHost, Ports, timeout)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// }
